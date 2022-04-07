0a26b0d0edb81864371c3af055985203ac39e778
mpetavy <mpetavy>
2022-04-07 08:22:15 +0200

    flag.parse missed

--- main.go
+++ main.go
@@ -6,10 +6,16 @@ import (
 )
 
 var (
-	timeout = flag.Int("t", 0, "timeout")
+	timeout = flag.Int("t", -1, "timeout")
 )
 
 func main() {
+	flag.Parse()
+
+	if *timeout == -1 {
+		flag.Usage()
+	}
+
 	t := time.Millisecond * time.Duration(*timeout)
 
 	time.Sleep(t)
