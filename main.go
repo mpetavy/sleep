3a8de6cd158faf2b3f41f87d9a6be639e1ca6d8c
mpetavy <mpetavy>
2022-04-07 08:17:04 +0200

    removed common

--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"github.com/mpetavy/common"
 	"time"
 )
 
@@ -11,22 +9,8 @@ var (
 	timeout = flag.Int("t", 0, "timeout")
 )
 
-func init() {
-	common.Init(false, "0.0.0", "", "", "2018", "test", "mpetavy", fmt.Sprintf("https://github.com/mpetavy/%s", common.Title()), common.APACHE, nil, nil, nil, run, 0)
-}
-
-func run() error {
-	t := common.MillisecondToDuration(*timeout)
-
-	common.Debug("sleep %v...", t)
-	time.Sleep(t)
-	common.Debug("continue")
-
-	return nil
-}
-
 func main() {
-	defer common.Done()
+	t := time.Millisecond * time.Duration(*timeout)
 
-	common.Run(nil)
+	time.Sleep(t)
 }
